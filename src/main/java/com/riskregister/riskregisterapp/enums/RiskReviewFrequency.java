f13b7aeecb3b13a9020781e03c3641cb99c07c86
Zaheer Baloch <zaheer@hey.com>
2026-08-22 03:07:21 +0300

    Changes

--- src/main/java/com/riskregister/riskregisterapp/enums/RiskReviewFrequency.java
+++ src/main/java/com/riskregister/riskregisterapp/enums/RiskReviewFrequency.java
@@ -1,7 +1,19 @@
 package com.riskregister.riskregisterapp.enums;
 
 public enum RiskReviewFrequency {
-	MONTHLY,
-	QUARTERLY,
-	YEARLY
+	WEEKLY("Weekly"),
+	MONTHLY("Monthly"),
+	QUARTERLY("Quarterly"),
+	SEMI_ANNUALLY("Semi-Annually"),
+	YEARLY("Yearly");
+
+	private final String displayName;
+
+	RiskReviewFrequency(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
 }
